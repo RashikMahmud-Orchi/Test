d32a1b07d4df8276bc4743c87934cf0c707532ba
RashikMahmud-Orchi <rashikmahmud13@gmail.com>
2024-05-24 01:13:40 -0300

    first commit

---
+++ firstcode.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+iris_data = np.loadtxt('iris.data',delimiter=',')
+print(iris_data.shape)
+print(iris_data)
+x=range(6)
+print(x)
+
+
+for i in range(5):
+    print (i);
+
+thelist=[1,2,3,4]
+print(thelist)
+len(thelist)
+print(type(thelist))
